f7200abac0c4ddd774b0028b1fa9347b9c175792
kiruushinde <ks0120291@gmail.com>
2024-02-26 13:44:17 +0530

    Finally Done with the project - stored api data using rxjs store

--- src/app/show-movie-details/show-movie-details.component.ts
+++ src/app/show-movie-details/show-movie-details.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit, inject } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { StoreMovieService } from '../services/store-movie.service';
+import { ApidataService } from '../apidata.service';
+import { Observable } from 'rxjs';
+import { Movie } from '../movie';
 
 @Component({
   selector: 'app-show-movie-details',
@@ -9,17 +12,24 @@ import { StoreMovieService } from '../services/store-movie.service';
 })
 export class ShowMovieDetailsComponent implements OnInit {
   movie: any;
+  movies: any;
 
   constructor(
     private route: ActivatedRoute,
-    private movieService: StoreMovieService
+    private movieService: StoreMovieService,
+    private apiMovie: ApidataService
   ) {}
 
   ngOnInit(): any {
-    let id: any = this.route.snapshot.paramMap.get('id');
-    // const newId = id.toString();
-    this.movie = this.movieService.getMovieById(id);
-    console.log(id, this.movie);
-    document.title = 'BestMovies - Movie ' + id;
+    // let id: any = this.route.snapshot.paramMap.get('id');
+    // // const newId = id.toString();
+    // this.movie = this.movieService.getMovieById(id);
+    // console.log(id, this.movie);
+    // document.title = 'BestMovies - Movie ' + id;
+
+    const id2: any = this.route.snapshot.paramMap.get('id');
+    this.movies = this.movieService.getMovieById(id2);
+
+    console.log('the current movie is : ', this.movies);
   }
 }
